59e535295116c70822840748f4ee4fbbc20731fd
Shayanin Vanichrudee <shayanin.van@gmail.com>
2026-09-21 13:31:47 +0700

    fix: eng menu

--- src/Experience/World/FieldVect.js
+++ src/Experience/World/FieldVect.js
@@ -1,89 +1,96 @@
-import Experience from '../Experience.js'
-import * as THREE from 'three'
+import Experience from "../Experience.js";
+import * as THREE from "three";
 
 export default class FieldVect {
-    constructor() {
-        this.experience = new Experience()
-        this.scene = this.experience.scene
-        this.world = this.experience.world
-        this.resources = this.experience.resources
-        this.ui = this.experience.debug.toolsPage
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.world = this.experience.world;
+    this.resources = this.experience.resources;
+    this.ui = this.experience.debug.toolsPage;
 
-        // Parameters
-        this.isShowed = false
-        this.length = 0.04
-        this.color = 'green'
-        this.headLength = 0.01
-        this.headWidth = 0.008
+    // Parameters
+    this.isShowed = false;
+    this.length = 0.04;
+    this.color = "green";
+    this.headLength = 0.01;
+    this.headWidth = 0.008;
 
-        // UI
-        this.ui.addBinding(this, 'isShowed', {
-            label: 'เวกเตอร์สนาม'
-        })
+    // UI
+    this.ui.addBinding(this, "isShowed", {
+      label: "field vector",
+    });
 
-        // Label
-        this.labelGeometry = new THREE.BufferGeometry().setAttribute('position', new THREE.BufferAttribute(new Float32Array([0, 0.8, 0]), 3))
-        this.labelMaterial = new THREE.PointsMaterial({
-            size: 0.08,
-            sizeAttenuation: true,
-            color: this.color,
-            alphaMap: this.resources.items.fieldLabelTexture,
-            transparent: true,
-        })
+    // Label
+    this.labelGeometry = new THREE.BufferGeometry().setAttribute(
+      "position",
+      new THREE.BufferAttribute(new Float32Array([0, 0.8, 0]), 3),
+    );
+    this.labelMaterial = new THREE.PointsMaterial({
+      size: 0.08,
+      sizeAttenuation: true,
+      color: this.color,
+      alphaMap: this.resources.items.fieldLabelTexture,
+      transparent: true,
+    });
 
-        this.setModel()
+    this.setModel();
+  }
+
+  setModel() {
+    this.vect1 = new THREE.ArrowHelper(
+      new THREE.Vector3(0, 0, -1),
+      new THREE.Vector3(0, 0, 0),
+      this.length,
+      this.color,
+      this.headLength,
+      this.headWidth,
+    );
+    this.vect2 = new THREE.ArrowHelper(
+      new THREE.Vector3(0, 0, -1),
+      new THREE.Vector3(0, 0, 0),
+      this.length,
+      this.color,
+      this.headLength,
+      this.headWidth,
+    );
+
+    this.vect1Label = new THREE.Points(this.labelGeometry, this.labelMaterial);
+    this.vect2Label = new THREE.Points(this.labelGeometry, this.labelMaterial);
+
+    this.vect1.cone.add(this.vect1Label);
+    this.vect2.cone.add(this.vect2Label);
+
+    this.scene.add(this.vect1);
+    this.scene.add(this.vect2);
+
+    this.hide();
+  }
+
+  update() {
+    // position
+    this.vect1.position.copy(
+      this.world.rightHands.hand1.getWorldPosition(new THREE.Vector3(0, 0, 0)),
+    );
+    this.vect2.position.copy(
+      this.world.rightHands.hand2.getWorldPosition(new THREE.Vector3(0, 0, 0)),
+    );
+
+    // Visibility
+    if (this.isShowed) {
+      this.show();
+    } else {
+      this.hide();
     }
+  }
 
-    setModel() {
-        this.vect1 = new THREE.ArrowHelper(
-            new THREE.Vector3(0, 0, -1),
-            new THREE.Vector3(0, 0, 0),
-            this.length,
-            this.color,
-            this.headLength,
-            this.headWidth
-        )
-        this.vect2 = new THREE.ArrowHelper(
-            new THREE.Vector3(0, 0, -1),
-            new THREE.Vector3(0, 0, 0),
-            this.length,
-            this.color,
-            this.headLength,
-            this.headWidth
-        )
+  hide() {
+    this.vect1.visible = false;
+    this.vect2.visible = false;
+  }
 
-        this.vect1Label = new THREE.Points(this.labelGeometry, this.labelMaterial)
-        this.vect2Label = new THREE.Points(this.labelGeometry, this.labelMaterial)
-
-        this.vect1.cone.add(this.vect1Label)
-        this.vect2.cone.add(this.vect2Label)
-
-        this.scene.add(this.vect1)
-        this.scene.add(this.vect2)
-
-        this.hide()
-    }
-
-    update() {
-        // position
-        this.vect1.position.copy(this.world.rightHands.hand1.getWorldPosition(new THREE.Vector3(0, 0, 0)))
-        this.vect2.position.copy(this.world.rightHands.hand2.getWorldPosition(new THREE.Vector3(0, 0, 0)))
-
-        // Visibility
-        if (this.isShowed) {
-            this.show()
-        } else {
-            this.hide()
-        }
-    }
-
-    hide() {
-        this.vect1.visible = false
-        this.vect2.visible = false
-    }
-
-    show() {
-        this.vect1.visible = true
-        this.vect2.visible = true
-    }
+  show() {
+    this.vect1.visible = true;
+    this.vect2.visible = true;
+  }
 }
--- src/Experience/World/RightHands.js
+++ src/Experience/World/RightHands.js
@@ -1,84 +1,89 @@
-import Experience from '../Experience.js'
-import BakedMaterial from './BakedMaterial.js'
+import Experience from "../Experience.js";
+import BakedMaterial from "./BakedMaterial.js";
 
 export default class RightHands {
-    constructor() {
-        this.experience = new Experience()
-        this.scene = this.experience.scene
-        this.resources = this.experience.resources
-        this.world = this.experience.world
-        this.ui = this.experience.debug.toolsPage
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+    this.world = this.experience.world;
+    this.ui = this.experience.debug.toolsPage;
 
-        // Resource
-        this.modelResource = this.resources.items.motorSceneModel
+    // Resource
+    this.modelResource = this.resources.items.motorSceneModel;
 
-        // Texture
-        this.bakedMaterial = new BakedMaterial()
-        this.texture = this.bakedMaterial.texture
+    // Texture
+    this.bakedMaterial = new BakedMaterial();
+    this.texture = this.bakedMaterial.texture;
 
-        // Info
-        this.leftRotorZPos = 0.1
-        this.rightRotorZPos = -0.1
+    // Info
+    this.leftRotorZPos = 0.1;
+    this.rightRotorZPos = -0.1;
 
-        // Parameters
-        this.isShowed = false
-        this.addHandRotation = 0 // additional rotation for learning tool
+    // Parameters
+    this.isShowed = false;
+    this.addHandRotation = 0; // additional rotation for learning tool
 
-        // UI
-        this.ui.addBinding(this, 'isShowed', {
-            label: 'มือขวา'
-        })
+    // UI
+    this.ui.addBinding(this, "isShowed", {
+      label: "right hands",
+    });
 
-        this.setModel()
+    this.setModel();
+  }
+
+  setModel() {
+    this.modelResource.scene.traverse((child) => {
+      if (child.name === "hand1") {
+        this.hand1 = child;
+        this.hand1.material = this.texture;
+      }
+      if (child.name === "hand2") {
+        this.hand2 = child;
+        this.hand2.material = this.texture;
+      }
+    });
+
+    this.hand1.position.y = 0;
+    this.hand2.position.y = 0;
+
+    this.world.rotor.model.add(this.hand1);
+    this.world.rotor.model.add(this.hand2);
+
+    this.hide();
+  }
+
+  update() {
+    this.hand1.rotation.x =
+      -this.world.rotor.model.rotation.x + this.addHandRotation;
+    this.hand2.rotation.x =
+      -this.world.rotor.model.rotation.x + this.addHandRotation;
+
+    if (
+      Math.abs(this.world.rotor.model.rotation.x) < Math.PI / 2 ||
+      Math.abs(this.world.rotor.model.rotation.x) > (3 * Math.PI) / 2
+    ) {
+      this.hand1.position.z = this.leftRotorZPos;
+      this.hand2.position.z = this.rightRotorZPos;
+    } else {
+      this.hand1.position.z = this.rightRotorZPos;
+      this.hand2.position.z = this.leftRotorZPos;
     }
 
-    setModel() {
-        this.modelResource.scene.traverse((child) => {
-            if (child.name === 'hand1') {
-                this.hand1 = child
-                this.hand1.material = this.texture
-            }
-            if (child.name === 'hand2') {
-                this.hand2 = child
-                this.hand2.material = this.texture
-            }
-        })
-
-        this.hand1.position.y = 0
-        this.hand2.position.y = 0
-
-        this.world.rotor.model.add(this.hand1)
-        this.world.rotor.model.add(this.hand2)
-
-        this.hide()
+    if (this.isShowed && this.world.switch.isOn) {
+      this.show();
+    } else {
+      this.hide();
     }
+  }
 
-    update() {
-        this.hand1.rotation.x = -this.world.rotor.model.rotation.x + this.addHandRotation
-        this.hand2.rotation.x = -this.world.rotor.model.rotation.x + this.addHandRotation
+  hide() {
+    this.hand1.visible = false;
+    this.hand2.visible = false;
+  }
 
-        if (Math.abs(this.world.rotor.model.rotation.x) < Math.PI/2 || Math.abs(this.world.rotor.model.rotation.x) > 3*Math.PI/2) {
-            this.hand1.position.z = this.leftRotorZPos
-            this.hand2.position.z = this.rightRotorZPos
-        } else {
-            this.hand1.position.z = this.rightRotorZPos
-            this.hand2.position.z = this.leftRotorZPos
-        }
-
-        if (this.isShowed && this.world.switch.isOn) {
-            this.show()
-        } else {
-            this.hide()
-        }
-    }
-
-    hide() {
-        this.hand1.visible = false
-        this.hand2.visible = false
-    }
-
-    show() {
-        this.hand1.visible = true
-        this.hand2.visible = true
-    }
+  show() {
+    this.hand1.visible = true;
+    this.hand2.visible = true;
+  }
 }
